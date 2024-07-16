03cb43c800b66b7109238144bb1fccc292ce60a0
Vahe Karapetyan <vaheek@Vahes-MacBook-Air.local>
2024-07-16 18:05:26 +0400

    IndexMapperAlgorithm that does not work fully. Exploring other possible ways to solve this.

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,22 @@
+import algorithms.IndexMapperAlgorithm;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        String quests = "bbbbbbbbbsbbbbbbbbsbbbsbbbsbbbbsbsbbsbsbbsbbsbsbsbsbssbsbsbsbsbsssbsssbssbssbsssssbssssssbssssssssss";
+        final String quests = "bbbbbbbbbsbbbbbbbbsbbbsbbbsbbbbsbsbbsbsbbsbbsbsbsbsbssbsbsbsbsbsssbsssbssbssbsssssbssssssbssssssssss";
 
         System.out.println("Hey!");
-//        System.out.println("Hey! Enter a number of quests for your game: ");
-//        System.out.print("==> ");
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
+        System.out.println("Enter a number of quests for your game: ");
+        System.out.print("==> ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        List<Integer> silverIndexes = findSilverIndexes(quests);
+        IndexMapperAlgorithm.generateRewards(quests, n);
 
-//        String generatedQuests = generateRewards(quests, n);
-//
-//        System.out.println("Here are the generated quests for your game!");
-//        System.out.print("==> ");
-//        System.out.println(generatedQuests);
-
-//        sc.close();
+        sc.close();
 
     }
-
-    private static List<Integer> findSilverIndexes(String quests) {
-        List<Integer> silverIndexes = new ArrayList<>();
-        for (int i = 0; i<quests.length(); i++) {
-            if (quests.charAt(i) == 's') {
-                silverIndexes.add(i);
-            }
-        }
-        System.out.println("Silver quests for [" + quests + "] are distributed at " + silverIndexes + " indexes.");
-        double silverPercentage = percentageOfSilvers(silverIndexes.size(), quests.length());
-        System.out.println("The " + silverPercentage + "% of the initial quests are silver.");
-        System.out.println("Based on this, let's find out the distribution for your quests and generate the output.");
-        return silverIndexes;
-    }
-
-    private static double percentageOfSilvers(double silversCount, double count) {
-        return (silversCount * 100) / count;
-    }
-
-//    private static String generateRewards(String sequence, int count) {
-//        String rewards = "b".repeat(count);
-//        if (count == sequence.length()) {
-//            rewards = sequence;
-//        }
-//        return rewards;
-//    }
 }
---
+++ src/algorithms/IndexMapperAlgorithm.java
@@ -0,0 +1,69 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IndexMapperAlgorithm {
+
+    /*
+    * NOTE
+    * This works only for lower numbers divisible by our example string's size.
+    * Halfway through it was notable that this algorithm is probably not the best way to solve the problem.
+    * */
+    public static String generateRewards(String quests, int n) {
+
+        System.out.println("For " + quests.length() + " quests we have this distribution:\n[" + quests + "]");
+
+        List<Integer> silverIndexes = findGivenSilverIndexes(quests);
+        int ratio = quests.length()/n;
+
+        List<Integer> newSilverIndexes = findNewSilverIndexes(silverIndexes, ratio, n);
+
+        return generateRewardsInternal(newSilverIndexes, n);
+    }
+
+    private static List<Integer> findGivenSilverIndexes(String quests) {
+        List<Integer> silverIndexes = new ArrayList<>();
+        for (int i = 0; i<quests.length(); i++) {
+            if (quests.charAt(i) == 's') {
+                silverIndexes.add(i);
+            }
+        }
+        System.out.println("Silver quests for [" + quests + "]\n are distributed at " + silverIndexes + " indexes.");
+        double silverPercentage = percentageOfSilvers(silverIndexes.size(), quests.length());
+        System.out.println("The " + silverPercentage + "% of the initial quests are silver.");
+        System.out.println("Based on this, let's find out the distribution for your quests and generate the output.");
+
+        System.out.println("---------------------------------------------------------------------------------------");
+
+        return silverIndexes;
+    }
+
+    private static List<Integer> findNewSilverIndexes(List<Integer> givenIndexes, int ratio, int n) {
+        List<Integer> newSilverIndexes = new ArrayList<>();
+        int i = 0;
+//        for (int i = 0; i < givenIndexes.size(); i += ratio) {
+        while (i < givenIndexes.size()) {
+            newSilverIndexes.add(givenIndexes.get(i)/ratio);
+            i += ratio;
+        }
+        System.out.println("Silver quests for our game should be distributed at " + newSilverIndexes + " indexes.");
+        double silverPercentage = percentageOfSilvers(newSilverIndexes.size(), n);
+        System.out.println("The " + silverPercentage + "% of the generated quests are silver.");
+        return newSilverIndexes;
+    }
+
+    private static double percentageOfSilvers(double silversCount, double count) {
+        return (silversCount * 100) / count;
+    }
+
+    private static String generateRewardsInternal(List<Integer> silverIndexes, int count) {
+        StringBuilder rewards = new StringBuilder("b".repeat(count));
+        for (Integer silverIndex : silverIndexes) {
+            rewards.setCharAt(silverIndex, 's');
+        }
+        System.out.println("Here is the generated sequence for " + count + " quests:");
+        System.out.println(">>> " + rewards);
+        return rewards.toString();
+    }
+}
